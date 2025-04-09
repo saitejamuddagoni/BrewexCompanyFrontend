92f77eadbfaac03e15ad0ce14559b883de6ca967
saitejamuddagoni <saitejamuddagoni@gmail.com>
2025-04-09 19:44:34 +0530

    Initial Commits for frontend

--- server/BrewexCompany/src/main/java/com/brewex/www/repo/HeadingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.brewex.www.repo;
-
-import com.brewex.www.model.Heading;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeadingRepository extends JpaRepository<Heading, Long> {
-}
-
